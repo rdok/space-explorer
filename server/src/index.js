66c11b0319ee0db910d03e0bacb59dae51b8278b
Rizart Dokollari <r.dokollari@gmail.com>
2019-10-19 05:54:23 +0100

    Add Launch, Mission, Rocket, and TripUpdateResponse types.

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,8 @@
 const { ApolloServer } = require('apollo-server')
-const schema = require('./schema')
+const typeDefs = require('./schema')
 
-const server = new ApolloServer({ schema })
+const server = new ApolloServer({ typeDefs })
+
+server.listen().then(({ url }) => {
+      console.log(`異Server ready at ${url}`);
+})
