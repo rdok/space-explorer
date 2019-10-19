42ab172697e6e1c98f01b4f0f8c2db43a5824423
Rizart Dokollari <r.dokollari@gmail.com>
2019-10-19 22:35:37 +0100

    Add ability to paginate launches.

--- api/src/schema.js
+++ api/src/schema.js
@@ -3,10 +3,29 @@ const { gql } = require('apollo-server')
 const typeDefs = gql`
 
 type Query {
-    launches: [Launch]!
+    launches(
+        """
+        The number of results to show. Must be >= 1. Default = 20
+        """
+        pageSize: Int
+        """
+        If you add a cursor here, it will only return results _after_ this cursor
+        """
+        after: String
+    ): LaunchConnection!
     launch(id: ID!): Launch
-    # Queries for the current user
     me: User
+}
+
+"""
+Simple wrapper around our list of launches that contains a cursor to the
+last item in the list. Pass this cursor to the launches query to fetch results
+after these.
+"""
+type LaunchConnection {
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
 }
 
 type Mutation {
