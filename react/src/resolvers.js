458cbbfc355fcbf9de4a1932fa6a2336be1f9a89
Rizart Dokollari <r.dokollari@gmail.com>
2019-10-20 14:23:20 +0100

    Add assets & bootstrap react tests.

--- react/src/resolvers.js
+++ react/src/resolvers.js
@@ -1,4 +1,5 @@
 import gql from 'graphql-tag'
+import { GET_CART_ITEMS } from './pages/cart'
 
 export const schema = gql`
     extend type Launch {
@@ -26,6 +27,18 @@ export const resolvers = {
         isInCart: (launch, _, { cache }) => {
             const { cartItems } = cache.readQuery({ query: GET_CART_ITEMS })
             return cartItems.includes(launch.id)
+        }
+    },
+    Mutation: {
+        addOrRemoveFromCart: (_, { id }, { cache }) => {
+            const { cartItems } = cache.readQuery({ query: GET_CART_ITEMS })
+            const data = {
+                cartItems: cartItems.includes(id)
+                    ? cartItems.filter(index => index !== id)
+                    : [...cartItems, id]
+            }
+            cache.writeQuery({ query: GET_CART_ITEMS, data })
+            return data.cartItems
         }
     }
 }
