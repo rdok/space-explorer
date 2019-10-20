9d80a16942afa4bead863a1f2cc7b579265311cd
Rizart Dokollari <r.dokollari@gmail.com>
2019-10-20 13:01:27 +0100

    Direct cache write.

--- react/src/containers/logout-button.js
+++ react/src/containers/logout-button.js
@@ -1,1 +1,27 @@
-import React 
+import React from 'react'
+import styled from 'react-emotion'
+import { useApolloClient } from '@apollo/react-hooks'
+
+import { menuItemsClassName } from '../components/menu-item'
+import { ReactComponent as ExitIcon } from '../assets/icons/exit.svg'
+
+export default function LogoutButton() { 
+    const client = useApolloClient()
+
+    return (
+        <StyleButton
+            onClick={() => {
+                client.writeData({ data: { isLoggedIn: false }})
+                localStorage.clear()
+            }}
+        >
+            <ExitIcon />
+        </StyleButton>
+    )
+}
+
+const StyleButton = styled('button')(menuItemClassName, {
+    background: 'none',
+    border: 'none',
+    padding: 0
+})
