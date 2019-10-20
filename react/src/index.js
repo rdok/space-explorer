365371629713891c86a3ab7b145c09bebaaab6d6
Rizart Dokollari <r.dokollari@gmail.com>
2019-10-20 12:50:05 +0100

    Add virtual fields to server data.

--- react/src/index.js
+++ react/src/index.js
@@ -1,4 +1,5 @@
 import { ApolloProvider } from '@apollo/react-hooks'
+import { useQuery } from '@apollo/react-hooks'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Pages from './pages'
@@ -6,16 +7,38 @@ import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 import gql from "graphql-tag"
+import { resolvers, typeDefs } from './resolvers'
 
 const cache = new InMemoryCache()
 const link = new HttpLink({
     uri: 'http://localhost:4000/',
     headers: { authorization: localStorage.getItem('token') }
 })
-const client = new ApolloClient({ cache, link })
+
+const client = new ApolloClient({ cache, link, typeDefs, resolvers })
+
+cache.writeData({
+    data: {
+        isLoggedIn: !!localStorage.getItem('token'),
+        cartItems: []
+    }
+})
 
 ReactDOM.render(
     <ApolloProvider client={client}>
     </ApolloProvider>,
     document.getElementById('root')
 )
+
+
+const IS_LOGGED_IN = gql`
+    query IsUserLoggedIn {
+        isLoggedIn @client
+    }
+`
+
+function IsLoggedIn() {
+    const { data } = useQuery( IS_LOGGED_IN )
+    console.log(data)
+    return data.isLoggedIn ? <Pages /> : <Login />
+}
