97847c9a1b42b4804aa5dd678a17bfe1d3648c9e
Rizart Dokollari <r.dokollari@gmail.com>
2019-10-22 07:26:55 +0100

    Assert resolvers.Mutation.cancelTrip may handle (un)successfull trip canceling.

--- api/src/__tests__/resolvers.mutation.js
+++ api/src/__tests__/resolvers.mutation.js
@@ -19,17 +19,17 @@ describe('[Mutation.bookTrips]', () => {
     const { bookTrips } = mockedContext.dataSources.userAPI
     const { getLaunchesByIds } = mockedContext.dataSources.launchAPI
 
-    it('may identify if booking succeeded', async () => {
+    it('may handle successfully trip booking', async () => {
         bookTrips.mockReturnValueOnce( [ { launchId: 1972 } ] )
 
         getLaunchesByIds
             .mockReturnValueOnce( [ { id: 1972, cursor: 'apollo-csm-114' } ] )
 
-        const bookedTrips = await resolvers.Mutation.bookTrips(
+        const response = await resolvers.Mutation.bookTrips(
             null, { launchIds: [ 1972 ] }, mockedContext
         )
 
-        expect( bookedTrips ).toEqual( { 
+        expect( response ).toEqual( { 
             launches: [ { cursor: 'apollo-csm-114', id: 1972 } ],
             message: 'trips booked successfully',
             success: true
@@ -38,17 +38,51 @@ describe('[Mutation.bookTrips]', () => {
         expect( bookTrips ).toBeCalledWith( { launchIds: [ 1972 ] } )
     })
 
-    it('may identify if a trip booking failed', async () => {
+    it('may handle faled trip booking', async () => {
         bookTrips.mockReturnValueOnce([])
 
-        const bookedTrips = await resolvers.Mutation.bookTrips(
+        const response = await resolvers.Mutation.bookTrips(
             null, { launchIds: [ 1972 ] }, mockedContext
         )
 
-        expect(bookedTrips.message).toBeDefined()
-        expect(bookedTrips.message).toEqual(
+        expect(response.message).toBeDefined()
+        expect(response.message).toEqual(
             `the following launches couldn't be booked: 1972`
         )
-        expect(bookedTrips.success).toBeFalsy()
+        expect(response.success).toBeFalsy()
+    })
+})
+
+describe('[Mutation.cancelTrip', () => {
+    const { cancelTrip } = mockedContext.dataSources.userAPI
+    const { getLaunchById } = mockedContext.dataSources.launchAPI
+
+    it('may handle successfull trip cancelation', async() => {
+        cancelTrip.mockReturnValueOnce(true)
+        getLaunchById
+            .mockReturnValueOnce( { id: 1968, cursor: 'apollo-8' } )
+
+        const response = await resolvers.Mutation.cancelTrip(
+            null, { launchId: 1972 }, mockedContext
+        )
+
+        expect( response ).toEqual({
+            success: true,
+            message: 'trip canceled',
+            launches: [ { id: 1968, cursor: 'apollo-8' } ]
+        })
+
+        expect( cancelTrip ).toBeCalledWith( { launchId: 1972 } )
+    })
+
+    it('may handle failed trip cancelation', async() => {
+        cancelTrip.mockReturnValueOnce(false)
+
+        const response = await resolvers.Mutation.cancelTrip(
+            null, { launchId: 1972 }, mockedContext
+        )
+
+        expect( response.message ).toEqual('failed to cancel trip')
+        expect( response.success ).toBeFalsy()
     })
 })
