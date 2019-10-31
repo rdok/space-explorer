788ff6cc840707ceca4e8ffe65feb249a14b19d5
Rizart Dokollari <r.dokollari@gmail.com>
2019-10-31 17:13:38 +0000

    Bootstrap user resolver unit test

---
+++ api/src/__tests__/resolvers.user.js
@@ -0,0 +1,22 @@
+const resolvers = require('../resolvers')
+
+describe('[User.trips]', () => {
+    const mockContext = {
+        dataSources: {
+            userAPI: { getLaunchIdsByUser: jest.fn() },
+            launchAPI: { getLaunchesByIds: jest.fn() },
+        },
+        user: { id: 1 }
+    }
+
+    const { getLaunchIdsByUser } = mockContext.dataSources.userAPI
+    const { getLaunchesByIds } = mockContext.dataSources.launchAPI
+
+    it('uses user id from context to lookup trips', async() => {
+        getLaunchIdsByUser.mockReturnValueOnce( [ 1975 ] )
+        getLaunchesByIds.mockReturnValueOnce( [ { id: 1975 } ] )
+
+        const response = await resolvers.User.trips(null, null, mockContext)
+        expect(response).toEqual( [ { id: 1975 ] )
+    })
+})
