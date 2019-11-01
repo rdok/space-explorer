0ec0e345664412102bb946ce0250ee1819f14719
Rizart Dokollari <r.dokollari@gmail.com>
2019-11-01 16:02:02 +0000

    Assert resolver.Mutation.cancel trip may handle successful & failed onces.

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -57,17 +57,18 @@ module.exports = {
                 success: results && results.length === launchIds.length,
                 message: results.length === launchIds.length
                     ? 'trips booked successfully'
-                    : `the following launches couldn't be booked: 
-                        ${launchIds.filter( id => !results.includes(id) )}`,
+                    : `the following launches couldn't be booked: ${launchIds.filter( id => !results.includes(id) )}`,
                 launches
             }
         },
         cancelTrip: async (_, { launchId }, { dataSources }) => {
             const result = await dataSources.userAPI.cancelTrip({ launchId })
 
-            if( ! result ) return {
-                success: false,
-                message: 'failed to cancel trip'
+            if( ! result ) {
+                return {
+                    success: false,
+                    message: 'failed to cancel trip'
+                }
             }
 
             const launch = await dataSources.launchAPI
